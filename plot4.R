76c2628a7add753ac505738e2e2432ad9fd20484
ADYASHA <adyasha@ADYASHAs-MacBook-Air.local>
2020-10-03 12:06:32 +0530

    corrected plot 4

--- plot4.R
+++ plot4.R
@@ -1,8 +1,8 @@
-dt <- read.delim("household_power_consumption.txt", header = TRUE, sep = ';')
+dt <- read.delim("/Users/adyasha/Desktop/coursework/Exp_Data/household_power_consumption.txt", header = TRUE, sep = ';')
 dt2 <- subset(dt, Date== "1/2/2007" | Date== "2/2/2007")
 dateNtime <- strptime(paste(dt2$Date,dt2$Time, sep = " "), "%d/%m/%Y %H:%M:%S")
 
-png(file = "plot4.png")
+png(file = "plot41.png")
 
 par(mfcol = c(2,2))
 
@@ -16,15 +16,16 @@ plot(dateNtime, as.numeric(dt2$Global_active_power), type = "l",
      ylab = "Global Active Power (in killowatts)", xlab = " ")
 
 #plot2 i.e top-right
-plot(dateNtime,voltage,type = "l", xlab = "datetime", ylab = "Voltage")
-
-#plot3 i.e bottom-left
 plot(dateNtime,submetering1, type = "l", xlab = " ", ylab = "Energy sub metering")
 lines(dateNtime,submetering2, type = "l", col = "red")
 lines(dateNtime,submetering3, type = "l", col = "blue")
 
 legend("topright", c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"), 
        lty = 1, lwd = 2.5, col = c("Black","Red","Blue"))
+
+
+#plot3 i.e bottom-left
+plot(dateNtime,voltage,type = "l", xlab = "datetime", ylab = "Voltage")
 
 #plot4 i.e bottom-right
 plot(dateNtime, as.numeric(dt2$Global_reactive_power), type = "l", 
